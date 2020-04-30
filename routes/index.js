1cdbb5d4d502e9e0ca5475703e2515b8fff6ee10
joeydash <joeydash@saarang.org>
2020-04-30 15:48:32 +0530

    feat : fetch added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const fetch = require('node-fetch');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    fetch('https://hasura-test-surya.herokuapp.com/v1/graphql', {
+        method: 'POST', body: JSON.stringify({
+            query: 'mutation MyMutation {\n' +
+                '  insert_chatroom(objects: {}) {\n' +
+                '    affected_rows\n' +
+                '    returning {\n' +
+                '      id\n' +
+                '    }\n' +
+                '  }\n' +
+                '}',
+            variables: {}
+        })
+    })
+        .then(res => res.json()) // expecting a json response
+        .then(json => {
+            // console.log(json);
+            res.json(json);
+        })
+        .catch(e => {
+            // console.log(e);
+            res.json(e);
+        });
+
 });
 
 module.exports = router;
