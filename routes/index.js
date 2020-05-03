5408035c22b00daf9f055bd8f1171890c1a25bc5
Suryanshu Singh <suryanshu15@gmail.com>
2020-05-03 09:17:12 +0530

    added query for chatroom_user_map

--- routes/index.js
+++ routes/index.js
@@ -29,4 +29,27 @@ router.get('/', function (req, res, next) {
 
 });
 
+router.post('/', function (req, res, next) {
+        fetch('https://hasura-test-surya.herokuapp.com/v1/graphql', {
+            method: 'POST', body: JSON.stringify({
+                query: 'mutation MyMutation {\n' +
+                    '  insert_chatroom_user_map(objects: {chatroom_id: "5e0ff47b-4734-4909-8f3f-414c25e987c5", user_id: "1059cf37-7304-4890-8df2-34eeec7ffe93"}) {\n' +
+                    '    affected_rows\n' +
+                    '  }\n' +
+                    '}\n',
+                        variables: {}
+                    }) })
+            .then(res => res.json()) // expecting a json response
+            .then(json => {
+                console.log(json);
+                res.json(json);
+            })
+            .catch(e => {
+             console.log(e);
+             res.json(e);
+            });
+});
+
+
+
 module.exports = router;
